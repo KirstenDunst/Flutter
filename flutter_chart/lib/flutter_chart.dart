025721d6b962b8978dd4e4882a0c4312bb96d29c
曹世鑫 <cao_shixin@yahoo.com>
2020-03-27 17:48:06 +0800

    文件补充

---
+++ flutter_chart/lib/flutter_chart.dart
@@ -0,0 +1,15 @@
+library flutter_chart;
+
+export 'package:flutter_chart/chart/bean/chart_bean_focus.dart';
+export 'package:flutter_chart/chart/bean/chart_bean.dart';
+export 'package:flutter_chart/chart/bean/chart_pie_bean.dart';
+export 'package:flutter_chart/chart/view/chart_bar.dart';
+export 'package:flutter_chart/chart/view/chart_line.dart';
+export 'package:flutter_chart/chart/view/chart_pie.dart';
+export 'package:flutter_chart/chart/view/chart_line_focus.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
